5b9ca538c1999a0ad4d70e229d4200d966bebbc1
Daniel Carrión Martínez <danicm89@gmail.com>
2023-10-20 12:02:54 +0000

    bootstrap ok

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -6,11 +6,11 @@ import fondo from '../../img/fondo.jpg';
 
 const Home = () => {
 	return (
-		<div className="text-center" style={{ 
+		<div className="text-center" style={{
 			backgroundImage: `url(${fondo})`,
 			backgroundSize: 'cover',
 			height: '100vh'
-		  }}>
+		}}>
 			<h1 className="text-center">Hello </h1>
 			<a href="#" className="btn btn-success">
 				If you see this green button... bootstrap is working...
@@ -20,6 +20,14 @@ const Home = () => {
 				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
 				love!
 			</p>
+			<div className="card" style={{width: "18rem"}}>
+				<img src="..." className="card-img-top" alt="..."/>
+					<div className="card-body">
+						<h5 className="card-title">Card title</h5>
+						<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+						<a href="#" className="btn btn-primary">Go somewhere</a>
+					</div>
+			</div>
 		</div>
 	);
 };
